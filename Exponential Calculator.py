3995c351658b58039a0639c164cd57e3bd2ef438
Diego Emmanuel C Cordero <169147738+DiegoEmmanuelCCOrdero@users.noreply.github.com>
2024-05-18 04:48:05 +0800

    Add files via upload

---
+++ Exponential Calculator.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 03:38:55 2024
+
+@author: Jem Cordero
+"""
+
+print("Welcome to Exponential Calculator")
+print("")
+num=0
+
+while num==0:
+    base=int(input("Enter the base value(or exit press 0):"))
+    print("")
+    if base==0:
+        print("Thank you for using the Exponential Calculator")
+        print("Goodbye")
+        break
+    elif base!=0:
+        exponent=int(input("Enter the Exponential Value:"))
+        print("")
+        result=(base ** exponent)
+        print(base, "to the power of", exponent, "is", result)
+        print("")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
